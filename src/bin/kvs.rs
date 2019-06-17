99ced64c8bdf6fa3eeec505862f3d72b9e91dfc2
Paul Meng <me@paulme.ng>
2019-06-17 10:25:25 +0800

    Using clap to stub command line argument

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,14 @@
+use clap::App;
+use clap::load_yaml;
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yml");
+    let app_m = App::from_yaml(yaml).get_matches();
+
+    match app_m.subcommand_name() {
+        Some("get") => {}, 
+        Some("set") => {},
+        Some("remove") => {},
+        _ => {}
+    }
 }
